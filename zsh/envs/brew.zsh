28f37ecac7898e90a924a42134f848260d126537
Chris Hoffman <cehoffman@gmail.com>
2012-03-06 11:23:49 -0600

    Change how brew handles coreutils

--- zsh/envs/brew.zsh
+++ zsh/envs/brew.zsh
@@ -2,7 +2,7 @@ if [[ -a ~/.homebrew/bin/brew ]]; then
   function {
     local brew_prefix="$HOME/.homebrew"
     path=($brew_prefix/bin $path)
-    path=($brew_prefix/sbin $path)
+    path=($(brew --prefix coreutils)/libexec/gnubin $brew_prefix/sbin $path)
     manpath=($brew_prefix/share/man $manpath)
 
     # Homebrew Python setup
