9e722bb6a046a2c894589967c934010cc2f28034
Chris Hoffman <cehoffman@gmail.com>
2012-05-08 02:21:33 -0500

    Setup ruby includes from homebrew

--- zsh/envs/brew.zsh
+++ zsh/envs/brew.zsh
@@ -9,5 +9,12 @@ if [[ -a ~/.homebrew/bin/brew ]]; then
     # Homebrew Python setup
     path=($(python -c "from sys import prefix; print prefix")/bin $path)
     path=($brew_prefix/share/python $path)
+
+    # Add homebrew ruby compiles to path if they exist
+    local ruby_libs
+    ruby_libs=${(z)$(ruby -e "exec_prefix = RbConfig::CONFIG['exec_prefix']; puts ['sitelibdir', 'sitearchdir', 'sitedir', 'vendorlibdir', 'vendorarchdir', 'vendordir', 'rubylibdir', 'archdir'].map { |dir| RbConfig::CONFIG[dir].sub(exec_prefix, '$brew_prefix') }.select { |dir| File.directory?(dir) }.map { |dir| dir.gsub(' ', '\\\\ ') }.join(' ')")}
+    if [[ ${#ruby_libs} > 0 ]]; then
+      export RUBYOPT="${RUBYOPT} -I${(j: -I:)ruby_libs}"
+    fi
   }
 fi
