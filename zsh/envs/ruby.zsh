b641734299026c6f49568e59eb40851608149220
Chris Hoffman <cehoffman@gmail.com>
2012-05-21 14:45:58 -0500

    Use RUBYLIB over RUBYOPT

--- zsh/envs/ruby.zsh
+++ zsh/envs/ruby.zsh
@@ -49,14 +49,18 @@ fi
 # Load up any special local lib dirs if needed
 function {
   if [[ -z $LPK_PREFIX ]]; then
+    typeset -gUT RUBYLIB ruby_lib
+
     # Add homebrew ruby compiles to path if they exist
     local ruby_libs
     local ruby_path
-    local ruby_paths
+
     ruby_libs=${(z)$(ruby -e "require 'rbconfig' unless defined?(RbConfig); puts RbConfig::CONFIG['ruby_version'], RbConfig::CONFIG['sitearch']")}
+
     for ruby_path in $LPKG_PREFIX/lib/ruby{,/site_ruby,/vendor_ruby}; do
-      ruby_paths=($ruby_path(/) $ruby_path/$ruby_libs[1](/) $ruby_path/$ruby_libs[1]/$ruby_libs[2](/))
-      [[ $#ruby_paths > 0 ]] && RUBYOPT="${RUBYOPT} -I${(j: -I:)ruby_paths}"
+      ruby_lib=($ruby_path(/) $ruby_path/$ruby_libs[1](/) $ruby_path/$ruby_libs[1]/$ruby_libs[2](/) $ruby_lib)
     done
+
+    export RUBYLIB
   fi
 }
