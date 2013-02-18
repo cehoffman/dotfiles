9ef1e9e7ed068673ea1339a9ab4f02c8041f5276
Chris Hoffman <cehoffman@gmail.com>
2013-02-17 20:42:56 -0600

    Add gnome-keyring integration

---
+++ zsh/envs/gnome-keyring.zsh
@@ -0,0 +1,14 @@
+if [[ -x $(which gnome-keyring-daemon) ]]; then
+  [[ -f ~/.dbus-daemon ]] && source ~/.dbus-daemon
+  [[ -f ~/.gnome-keyring-daemon ]] && export $(cat ~/.gnome-keyring-daemon)
+
+  if [[ -z "$DBUS_SESSION_BUS_PID" || "$(kill -0 $DBUS_SESSION_BUS_PID; echo $?)" != "0" ]]; then
+    # gnome-keyring needs dbus for communication
+    eval $(dbus-launch --sh-syntax | tee ~/.dbus-daemon)
+  fi
+
+  if [[ -z "$GNOME_KEYRING_PID" || "$(kill -0 $GNOME_KEYRING_PID; echo $?)" != "0" ]]; then
+    # Let ssh-agent/gpg-agent take control of their tasks
+    export $(gnome-keyring-daemon --components=pkcs11,secrets | tee ~/.gnome-keyring-daemon)
+  fi
+fi
