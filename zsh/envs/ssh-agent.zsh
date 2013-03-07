a3ff5a18e188e445e78b35ad785f89ddd6a377a9
Chris Hoffman <cehoffman@gmail.com>
2013-03-07 15:08:02 -0600

    Only need sync-ssh-auth-sock inside tmux

--- zsh/envs/ssh-agent.zsh
+++ zsh/envs/ssh-agent.zsh
@@ -1,3 +1,12 @@
+if [[ -n $TMUX ]]; then
+  function sync-ssh-auth-sock() {
+    local NEW_SSH_AUTH_SOCK=$(command tmux showenv| grep \^SSH_AUTH_SOCK | cut -d = -f 2)
+    if [[ -n $NEW_SSH_AUTH_SOCK && -S $NEW_SSH_AUTH_SOCK ]]; then
+      export SSH_AUTH_SOCK="$NEW_SSH_AUTH_SOCK"
+    fi
+  }
+fi
+
 if [[ -z "$SSH_AUTH_SOCK" ]]; then
   [[ -f ~/.ssh-agent ]] && source ~/.ssh-agent
 
