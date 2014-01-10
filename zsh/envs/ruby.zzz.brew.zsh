9d68cac9cb49fe960bb2d440e593c61ba14f3804
Chris Hoffman <cehoffman@gmail.com>
2014-01-10 16:54:36 -0600

    Avoid missing dirs in path

--- zsh/envs/ruby.zzz.brew.zsh
+++ zsh/envs/ruby.zzz.brew.zsh
@@ -1,10 +1,13 @@
 if [[ -a ~/.homebrew/bin/brew ]]; then
   function {
     export LPKG_PREFIX="$HOME/.homebrew"
+    echo $path
     path=($LPKG_PREFIX/bin $path)
-    path=($(brew --prefix coreutils)/libexec/gnubin $LPKG_PREFIX/sbin $path)
-    manpath=($(brew --prefix coreutils)/libexec/gnuman $LPKG_PREFIX/share/man $manpath)
-    fpath=($LPKG_PREFIX/share/zsh/functions $fpath)
+    echo $path
+    path=($(brew --prefix coreutils)/libexec/gnubin(/) $LPKG_PREFIX/sbin(/) $path)
+    echo $path
+    manpath=($(brew --prefix coreutils)/libexec/gnuman(/) $LPKG_PREFIX/share/man(/) $manpath)
+    fpath=($LPKG_PREFIX/share/zsh/functions(/) $fpath)
 
     # Homebrew Python setup
     path=($(python -c "from sys import prefix; print prefix")/bin $path)
