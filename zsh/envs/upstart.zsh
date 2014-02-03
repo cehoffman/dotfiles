e6d5af908a645154233302d0ec667dc898c8db78
Chris Hoffman <cehoffman@gmail.com>
2014-02-03 11:42:28 +0000

    Add auto setup of upstart environment for shell

---
+++ zsh/envs/upstart.zsh
@@ -0,0 +1,7 @@
+if [[ -n "$XDG_RUNTIME_DIR" ]]; then
+  # export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+  . $XDG_RUNTIME_DIR/upstart/sessions/*.session
+  if [[ -n "$UPSTART_SESSION" ]]; then
+    export UPSTART_SESSION
+  fi
+fi
