fd28f232ede04ff8e437edc191677ea82034894b
Chris Hoffman <cehoffman@gmail.com>
2014-02-05 11:24:52 +0000

    Only source the newest upstart runtime

--- zsh/envs/upstart.zsh
+++ zsh/envs/upstart.zsh
@@ -1,6 +1,6 @@
 if [[ -n "$XDG_RUNTIME_DIR" ]]; then
   # export XDG_RUNTIME_DIR="/run/user/$(id -u)"
-  . $XDG_RUNTIME_DIR/upstart/sessions/*.session
+  . $XDG_RUNTIME_DIR/upstart/sessions/*.session(oc[1])
   if [[ -n "$UPSTART_SESSION" ]]; then
     export UPSTART_SESSION
   fi
