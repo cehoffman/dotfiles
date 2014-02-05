3bd266a2013052d612faf9322167d3fd3442df65
Chris Hoffman <cehoffman@gmail.com>
2014-02-05 12:46:19 +0000

    Only do ssh agent forwarding, no local agents

--- zsh/envs/ssh-agent.zsh
+++ zsh/envs/ssh-agent.zsh
@@ -1,18 +1,10 @@
-if [[ -n $TMUX ]]; then
-  function sync-ssh-auth-sock() {
-    local NEW_SSH_AUTH_SOCK=$(command tmux showenv| grep \^SSH_AUTH_SOCK | cut -d = -f 2)
-    if [[ -n $NEW_SSH_AUTH_SOCK && -S $NEW_SSH_AUTH_SOCK ]]; then
-      export SSH_AUTH_SOCK="$NEW_SSH_AUTH_SOCK"
-    fi
-  }
-fi
-
-if [[ -z "$SSH_AUTH_SOCK" ]]; then
-  [[ -f ~/.ssh-agent ]] && source ~/.ssh-agent
-
-  # If the agent isn't running or hasn't ever been set for this account
-  if [[ -z "$SSH_AGENT_PID" || "$(ps -p $SSH_AGENT_PID &> /dev/null; echo $?)" != "0" ]]; then
-    # start the agent and delete the last line (the echo line) and save to file
-    eval $(umask 066 && ssh-agent | sed '$d' | tee ~/.ssh-agent)
+if [[ -o interactive ]]; then
+  if [[ -n $TMUX ]]; then
+    function sync-ssh-auth-sock() {
+      local NEW_SSH_AUTH_SOCK=$(command tmux showenv| grep \^SSH_AUTH_SOCK | cut -d = -f 2)
+      if [[ -n $NEW_SSH_AUTH_SOCK && -S $NEW_SSH_AUTH_SOCK ]]; then
+        export SSH_AUTH_SOCK="$NEW_SSH_AUTH_SOCK"
+      fi
+    }
   fi
 fi
