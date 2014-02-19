ad26ffcdbce2718e4352bd95e581009c4b29398f
Chris Hoffman <cehoffman@gmail.com>
2014-02-18 20:48:51 -0600

    Add golang for zsh

---
+++ zsh/envs/go.zsh
@@ -0,0 +1,6 @@
+if [[ -x $(which go) ]]; then
+  for dir ($(go env GOROOT)/bin/*(/)) { dir=$(basename $dir); eval "alias go-${dir%_*}-${dir#*_}=\"GOOS=${dir%_*} GOARCH=${dir#*_} go\"" }
+  unset dir
+  export GOPATH=~/Projects/gocode
+  path=($GOPATH/bin $path)
+fi
