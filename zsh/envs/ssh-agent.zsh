5331c37965e21ed5dab5ca889442a5625ec6a99d
Chris Hoffman <cehoffman@gmail.com>
2014-02-22 18:26:37 -0600

    Condence tmux ssh-agent sync

--- zsh/envs/ssh-agent.zsh
+++ zsh/envs/ssh-agent.zsh
@@ -1,10 +1,8 @@
-if [[ -o interactive ]]; then
-  if [[ -n $TMUX ]]; then
-    function sync-ssh-auth-sock() {
-      local NEW_SSH_AUTH_SOCK=$(command tmux showenv| grep \^SSH_AUTH_SOCK | cut -d = -f 2)
-      if [[ -n $NEW_SSH_AUTH_SOCK && -S $NEW_SSH_AUTH_SOCK ]]; then
-        export SSH_AUTH_SOCK="$NEW_SSH_AUTH_SOCK"
-      fi
-    }
-  fi
+if [[ -o interactive && -n $TMUX ]]; then
+  function sync-ssh-auth-sock() {
+    local NEW_SSH_AUTH_SOCK=$(command tmux showenv| grep \^SSH_AUTH_SOCK | cut -d = -f 2)
+    if [[ -n $NEW_SSH_AUTH_SOCK && -S $NEW_SSH_AUTH_SOCK ]]; then
+      export SSH_AUTH_SOCK="$NEW_SSH_AUTH_SOCK"
+    fi
+  }
 fi
