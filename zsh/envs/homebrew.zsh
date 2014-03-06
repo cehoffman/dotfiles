7348667863ba92c04dd764d42627bba479e26ad5
Chris Hoffman <cehoffman@gmail.com>
2014-03-06 05:47:52 -0600

    Update zsh config for using pyenv

--- zsh/envs/homebrew.zsh
+++ zsh/envs/homebrew.zsh
@@ -1,16 +1,10 @@
-if [[ -a ~/.homebrew/bin/brew ]]; then
+if [[ -d ~/.homebrew ]]; then
   function {
-    local coreutils="$HOME/.homebrew/opt/coreutils" # $(brew --prefix coreutils)
+    local coreutils="$HOME/.homebrew/opt/coreutils"
     path=($HOME/.homebrew/bin $path)
     path=($coreutils/libexec/gnubin(/) $HOME/.homebrew/sbin(/) $path)
     manpath=($coreutils/libexec/gnuman(/) $HOME/.homebrew/share/man(/) $manpath)
     fpath=($HOME/.homebrew/share/zsh/functions(/) $fpath)
-
-    # Homebrew Python setup
-    # if [[ -d $HOME/.homebrew/Cellar/python ]]; then
-    #   path=($(python -c "from sys import prefix; print prefix")/bin $path)
-    #   path=($HOME/.homebrew/share/python $path)
-    # fi
 
     if [[ $IS_LINUX = 0 ]]; then
       typeset -xTgU LD_LIBRARY_PATH ld_library_path
--- zsh/envs/python.zsh
+++ zsh/envs/python.zsh
@@ -1,28 +1,9 @@
 if command -v virtualenv &> /dev/null; then
-  # Virtualenvwrapper
-  # export WORKON_HOME="$HOME/.virtualenvs"
-  # hash -d virtualenvs=$WORKON_HOME
-  # source $(which virtualenvwrapper.sh)
-
-  # Virtualenv
-  # export VIRTUALENV_USE_DISTRIBUTE=true
-
   # PIP config
   # export PIP_VIRTUALENV_BASE=$WORKON_HOME
-  export PIP_RESPECT_VIRTUALENV=true
-  export PIP_REQUIRE_VIRTUALENV=true
+  # export PIP_RESPECT_VIRTUALENV=true
+  # export PIP_REQUIRE_VIRTUALENV=true
   export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
 
-  function syspip() { PIP_REQUIRE_VIRTUALENV="" pip "$@" }
-
-  if [[ -f $WORKON_HOME/.active ]]; then
-    workon `cat $WORKON_HOME/.active`
-    cd ~
-  fi
+  # function syspip() { PIP_REQUIRE_VIRTUALENV="" pip "$@" }
 fi
-
-function virtualenv_prompt_info() {
-  if [[ -n $VIRTUAL_ENV ]]; then
-    echo "$ZSH_THEME_VENV_PROMPT_PREFIX$(basename ${(q)VIRTUAL_ENV})$ZSH_THEME_VENV_PROMT_SUFFIX"
-  fi
-}
