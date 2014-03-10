3cc9c4c5813beeeaa0ce31630a38cc0e934c547b
Chris Hoffman <cehoffman@gmail.com>
2014-03-10 07:21:20 -0500

    Make upstart session source not show errors

--- zsh/envs/upstart.zsh
+++ zsh/envs/upstart.zsh
@@ -1,7 +1,7 @@
-if [[ -n "$XDG_RUNTIME_DIR" ]]; then
-  # export XDG_RUNTIME_DIR="/run/user/$(id -u)"
-  . $XDG_RUNTIME_DIR/upstart/sessions/*.session(oc[1])
-  if [[ -n "$UPSTART_SESSION" ]]; then
-    export UPSTART_SESSION
+function {
+  if [[ -n "$XDG_RUNTIME_DIR" ]]; then
+    local session="$(echo $XDG_RUNTIME_DIR/upstart/sessions/*.session(oc[1]))"
+    [[ -n "$session" ]] && source "$session"
+    [[ -n "$UPSTART_SESSION" ]] && export UPSTART_SESSION
   fi
-fi
+}
