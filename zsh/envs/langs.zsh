5a80610b25e6e5bce8cb8a7a53a7e30a0ba85aa9
Chris Hoffman <cehoffman@gmail.com>
2014-03-14 11:56:51 -0500

    Setup shell integration for env languages
    This also causes MANPATH to be updated with the currently active language
    version so you get correct documentation.
    
    The shell integration is done this way to cut down on shell startup times due
    to the standard eval "$(rbenv init -)" taking a majority of the shell startup
    time.

--- zsh/envs/langs.zsh
+++ zsh/envs/langs.zsh
@@ -2,10 +2,33 @@ typeset -gA ENV_LANGS
 ENV_LANGS=(rb red lua cyan py yellow)
 function {
   local lang
+  local funs
   for lang in ${(k)ENV_LANGS}; do
     if [[ -x "$HOME/.${lang}env/bin/${lang}env" ]]; then
       path=("$HOME/.${lang}env/bin" "$HOME/.${lang}env/shims" $path)
       source "$HOME/.${lang}env/completions/${lang}env.zsh"
+      funs="
+      function ${lang}env() {
+        local cmd=\"\$1\"
+        if [ \"\$#\" -gt 0 ]; then
+          shift
+        fi
+        case \"\$cmd\" in
+          rehash|shell)
+            eval \"\$(${lang}env sh-\$cmd \"\$@\")\"
+          ;;
+          *)
+            command ${lang}env \"\$cmd\" \"\$@\"
+          ;;
+        esac
+      }
+      function __${lang}env_manpath() {
+        local mpath
+        for mpath (~/.${lang}env/versions/*(/)) manpath=(\"\${(@)manpath:#\$mpath/share/man}\")
+        manpath=(\"$HOME/.${lang}env/versions/\$(${lang}env version-name)/share/man\"(/) \$manpath)
+      }"
+      eval "$funs"
+      precmd_functions+=(__${lang}env_manpath)
     fi
   done
 }
