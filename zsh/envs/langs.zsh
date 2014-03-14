129586573d51b9acf31179331e9978437ac46d44
Chris Hoffman <cehoffman@gmail.com>
2014-03-14 11:59:41 -0500

    Make langs shell integration only on interactive

--- zsh/envs/langs.zsh
+++ zsh/envs/langs.zsh
@@ -7,28 +7,30 @@ function {
     if [[ -x "$HOME/.${lang}env/bin/${lang}env" ]]; then
       path=("$HOME/.${lang}env/bin" "$HOME/.${lang}env/shims" $path)
       source "$HOME/.${lang}env/completions/${lang}env.zsh"
-      funs="
-      function ${lang}env() {
-        local cmd=\"\$1\"
-        if [ \"\$#\" -gt 0 ]; then
-          shift
-        fi
-        case \"\$cmd\" in
-          rehash|shell)
-            eval \"\$(${lang}env sh-\$cmd \"\$@\")\"
-          ;;
-          *)
-            command ${lang}env \"\$cmd\" \"\$@\"
-          ;;
-        esac
-      }
-      function __${lang}env_manpath() {
-        local mpath
-        for mpath (~/.${lang}env/versions/*(/)) manpath=(\"\${(@)manpath:#\$mpath/share/man}\")
-        manpath=(\"$HOME/.${lang}env/versions/\$(${lang}env version-name)/share/man\"(/) \$manpath)
-      }"
-      eval "$funs"
-      precmd_functions+=(__${lang}env_manpath)
+      if [[ -o interactive ]]; then
+        funs="
+        function ${lang}env() {
+          local cmd=\"\$1\"
+          if [ \"\$#\" -gt 0 ]; then
+            shift
+          fi
+          case \"\$cmd\" in
+            rehash|shell)
+              eval \"\$(${lang}env sh-\$cmd \"\$@\")\"
+            ;;
+            *)
+              command ${lang}env \"\$cmd\" \"\$@\"
+            ;;
+          esac
+        }
+        function __${lang}env_manpath() {
+          local mpath
+          for mpath (~/.${lang}env/versions/*(/)) manpath=(\"\${(@)manpath:#\$mpath/share/man}\")
+          manpath=(\"$HOME/.${lang}env/versions/\$(${lang}env version-name)/share/man\"(/) \$manpath)
+        }"
+        eval "$funs"
+        precmd_functions+=(__${lang}env_manpath)
+      fi
     fi
   done
 }
