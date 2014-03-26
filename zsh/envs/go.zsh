3bc05154085e73a5513cda90941da51bfea5338f
Chris Hoffman <cehoffman@gmail.com>
2014-03-26 04:39:25 -0500

    Simplify go cross compile alias generation using zsh builtins

--- zsh/envs/go.zsh
+++ zsh/envs/go.zsh
@@ -1,5 +1,5 @@
 if (( $+commands[go] )); then
-  for dir ($(go env GOROOT)/bin/*(/)) { dir=$(basename $dir); eval "alias go-${dir%_*}-${dir#*_}=\"GOOS=${dir%_*} GOARCH=${dir#*_} go\"" }
+  for dir ($(go env GOROOT)/bin/*(/:t)) alias go-${dir%_*}-${dir#*_}="GOOS=${dir%_*} GOARCH=${dir#*_} go"
   unset dir
   export GOPATH=~/Projects/gocode
   path[1,0]=$GOPATH:q/bin
