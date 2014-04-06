490f072481715da7e04eebf7d1104d4597b9dcaf
Chris Hoffman <cehoffman@gmail.com>
2014-04-05 22:27:30 -0500

    Fix precedence order for home zsh function paths

--- zsh/envs/homebrew.zsh
+++ zsh/envs/homebrew.zsh
@@ -3,7 +3,16 @@ if [[ -d ~/.homebrew ]]; then
     local coreutils=~/.homebrew/opt/coreutils
     path[1,0]=($coreutils:q/libexec/gnubin(/) ~/.homebrew/sbin(/) ~/.homebrew/bin)
     manpath[1,0]=($coreutils:q/libexec/gnuman(/) ~/.homebrew/share/man(/))
-    fpath[1,0]=(~/.homebrew/share/zsh/functions(/))
+
+    # Reorder zsh function directories because homebrew inserts them in the
+    # wrong precedence order
+    local fps
+    local fp
+    fps=(~/.homebrew/share/zsh/functions(/)
+         ~/.homebrew/Cellar/zsh/$ZSH_VERSION/share/zsh/functions(/)
+         ~/.homebrew/share/zsh/site-functions(/))
+    for fp ($fps) fpath=("${(@)fpath:#$fp}")
+    fpath+=($fps)
 
     if [[ $IS_LINUX = 0 ]]; then
       typeset -xTgU LD_LIBRARY_PATH ld_library_path
