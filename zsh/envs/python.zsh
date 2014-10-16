af4fefad24447065678a8aeff0b502e85c2ce502
Chris Hoffman <cehoffman@gmail.com>
2014-10-16 10:28:41 +0800

    Add pip autocompletion

--- zsh/envs/python.zsh
+++ zsh/envs/python.zsh
@@ -2,3 +2,17 @@ if (( $+commands[virtualenv] )); then
   # PIP config
   export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
 fi
+
+if (( $+commands[pip] )); then
+  # pip zsh completion start
+  function _pip_completion {
+    local words cword
+    read -Ac words
+    read -cn cword
+    reply=( $( COMP_WORDS="$words[*]" \
+              COMP_CWORD=$(( cword-1 )) \
+              PIP_AUTO_COMPLETE=1 $words[1] ) )
+  }
+  compctl -K _pip_completion pip
+  # pip zsh completion end
+fi
