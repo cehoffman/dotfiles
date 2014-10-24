3a2b3cb721a3d23110a13c134ae56a42f118fddc
Chris Hoffman <cehoffman@gmail.com>
2014-10-24 08:55:25 +0800

    Setup gpg-agent use when backup is mounted

--- zsh/envs/gpg.zsh
+++ zsh/envs/gpg.zsh
@@ -1,19 +1,27 @@
-function start-gpg() {
-  if [[ -o interactive && $+commands[gpg-agent] -eq 1 ]]; then
-    eval $(gpg-agent --daemon --write-env-file --enable-ssh-support)
-  fi
-}
-
-if [[ -f ~/.gpg-agent-info ]]; then
-  eval "export ${(fj:\nexport :)$(cat ~/.gpg-agent-info)}"
-else
-  start-gpg
-fi
-
-if [[ -n "$GPG_AGENT_INFO" && ! "$(ps -p ${${(ps/:/)GPG_AGENT_INFO}[2]})" =~ '\sgpg-agent\s' ]]; then
-  start-gpg
-fi
-
-unfunction start-gpg
-
 export GPG_TTY=$(tty)
+
+if [[ -d "/Volumes/GPG Backup/gnupghome" ]]; then
+  export GNUPGHOME="/Volumes/GPG Backup/gnupghome"
+
+  [[ -S "$GNUPGHOME/S.gpg-agent" ]] || gpg-agent --daemon --enable-ssh-support
+  export SSH_AUTH_SOCK="$GNUPGHOME/S.gpg-agent.ssh"
+else
+  function start-gpg() {
+    if [[ -o interactive && $+commands[gpg-agent] -eq 1 ]]; then
+      eval $(gpg-agent --daemon --write-env-file --enable-ssh-support)
+    fi
+  }
+
+  if [[ -f ~/.gpg-agent-info ]]; then
+    eval "export ${(fj:\nexport :)$(cat ~/.gpg-agent-info)}"
+  else
+    start-gpg
+  fi
+
+  if [[ -n "$GPG_AGENT_INFO" && ! "$(ps -p ${${(ps/:/)GPG_AGENT_INFO}[2]})" =~ '\sgpg-agent\s' ]]; then
+    start-gpg
+  fi
+
+  unfunction start-gpg
+fi
+
