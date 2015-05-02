ba371166cecff4b516d8a94bba91747929e144e7
Chris Hoffman <cehoffman@gmail.com>
2015-05-02 14:24:20 -0500

    Defensive programming for null value

--- zsh/envs/ssh-agent.zsh
+++ zsh/envs/ssh-agent.zsh
@@ -1,7 +1,7 @@
 if [[ -o interactive && -n $TMUX ]]; then
   function sync-ssh-auth-sock() {
-    local NEW_SSH_AUTH_SOCK=$(command tmux showenv| grep \^SSH_AUTH_SOCK | cut -d = -f 2)
-    if [[ -n $NEW_SSH_AUTH_SOCK && -S $NEW_SSH_AUTH_SOCK ]]; then
+    local NEW_SSH_AUTH_SOCK="$(command tmux showenv| grep \^SSH_AUTH_SOCK | cut -d = -f 2)"
+    if [[ -n "$NEW_SSH_AUTH_SOCK" && -S "$NEW_SSH_AUTH_SOCK" ]]; then
       export SSH_AUTH_SOCK="$NEW_SSH_AUTH_SOCK"
     fi
   }
