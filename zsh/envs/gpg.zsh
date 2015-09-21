6d5371658804bbc815f770f002a1f06fb15e83b5
Chris Hoffman <cehoffman@gmail.com>
2015-09-21 00:44:42 -0500

    Simplify gpg-agent by assuming 2.1 series agent always

--- zsh/envs/gpg.zsh
+++ zsh/envs/gpg.zsh
@@ -2,26 +2,9 @@ export GPG_TTY=$(tty)
 
 if [[ -d "/Volumes/GPG Backup/gnupghome" ]]; then
   export GNUPGHOME="/Volumes/GPG Backup/gnupghome"
-# fi
-
-  [[ -S "${GNUPGHOME-$HOME/.gnupg}/S.gpg-agent" ]] || gpg-agent --daemon --enable-ssh-support &> /dev/null
-  export SSH_AUTH_SOCK="$GNUPGHOME/S.gpg-agent.ssh"
 else
-  function start-gpg() {
-    if [[ -o interactive && $+commands[gpg-agent] -eq 1 ]]; then
-      eval $(gpg-agent --daemon --write-env-file --enable-ssh-support)
-    fi
-  }
-
-  if [[ -f ~/.gpg-agent-info ]]; then
-    eval "export ${(fj:\nexport :)$(cat ~/.gpg-agent-info)}"
-  else
-    start-gpg
-  fi
-
-  if [[ -n "$GPG_AGENT_INFO" && ! "$(ps -p ${${(ps/:/)GPG_AGENT_INFO}[2]})" =~ '\sgpg-agent\s' ]]; then
-    start-gpg
-  fi
-
-  unfunction start-gpg
+  export GNUPGHOME="$HOME/.gnupg"
 fi
+
+[[ -S "$GNUPGHOME/S.gpg-agent" ]] || gpg-agent --daemon --enable-ssh-support &> /dev/null
+export SSH_AUTH_SOCK="$GNUPGHOME/S.gpg-agent.ssh"
