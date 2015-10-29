a84fc96dda30d558a5fe80460c0ffab684cfa9e8
Chris Hoffman <cehoffman@gmail.com>
2015-10-29 10:07:24 -0500

    Only run gpg config if gpg-agent exists

--- zsh/envs/gpg.zsh
+++ zsh/envs/gpg.zsh
@@ -1,10 +1,12 @@
-export GPG_TTY=$(tty)
+if (( $+commands[gpg-agent] )); then
+  export GPG_TTY=$(tty)
 
-if [[ -d "/Volumes/GPG Backup/gnupghome" ]]; then
-  export GNUPGHOME="/Volumes/GPG Backup/gnupghome"
-else
-  export GNUPGHOME="$HOME/.gnupg"
+  if [[ -d "/Volumes/GPG Backup/gnupghome" ]]; then
+    export GNUPGHOME="/Volumes/GPG Backup/gnupghome"
+  else
+    export GNUPGHOME="$HOME/.gnupg"
+  fi
+
+  [[ -S "$GNUPGHOME/S.gpg-agent" ]] || gpg-agent --daemon --enable-ssh-support &> /dev/null
+  export SSH_AUTH_SOCK="$GNUPGHOME/S.gpg-agent.ssh"
 fi
-
-[[ -S "$GNUPGHOME/S.gpg-agent" ]] || gpg-agent --daemon --enable-ssh-support &> /dev/null
-export SSH_AUTH_SOCK="$GNUPGHOME/S.gpg-agent.ssh"
