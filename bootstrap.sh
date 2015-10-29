992b0dd8c2b76213927949f3bf85ba12c9920154
Chris Hoffman <cehoffman@gmail.com>
2015-10-29 11:03:40 -0500

    Use more recent openssl with git

--- bootstrap.sh
+++ bootstrap.sh
@@ -129,7 +129,7 @@ elif [ "$os" = "darwin" ]; then
   brew install nginx --with-libressl
   brew install gnu-sed gnu-tar reattach-to-user-namespace
 fi
-brew install git --with-pcre --with-persistent-https --with-brewed-curl
+brew install git --with-pcre --with-persistent-https --with-brewed-curl --with-brewed-openssl
 brew install git-extras zsh ctags cpanminus stderred tmux the_silver_searcher
 
 if [ "$os" = "linux" ]; then
