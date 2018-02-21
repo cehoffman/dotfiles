2bd1330a2f32bba55ad838119e0baa3f050a3ab1
Chris Hoffman <cehoffman@gmail.com>
2018-02-21 14:54:55 -0600

    Fully remove the old *env setup code

--- zsh/envs/langs.zsh
+++ zsh/envs/langs.zsh
@@ -31,42 +31,4 @@ function {
       done
     fi
   fi
-
-  for lang in ${(k)ENV_LANGS}; do
-    if [[ -x "$HOME/.${lang}env/bin/${lang}env" ]]; then
-      path[1,0]=(~/.${lang}env/bin ~/.${lang}env/shims)
-      export ${(U)lang}ENV_SHELL=zsh
-      export ${(U)lang}ENV_ROOT="${HOME}/.${lang}env"
-      source ~/.${lang}env/completions/${lang}env.zsh
-      if [[ -o interactive ]]; then
-        shlist=(~/.${lang}env/libexec/${lang}env-sh-*(:t) ~/.${lang}env/plugins/*/bin/${lang}env-sh-*(:t))
-        funs="
-        function ${lang}env() {
-          local cmd=\"\$1\"
-          if [ \"\$#\" -gt 0 ]; then
-            shift
-          fi
-          case \"\$cmd\" in
-            ${(j:|:)shlist//${lang}env-sh-})
-              eval \"\$(${lang}env sh-\$cmd \"\$@\")\"
-            ;;
-            *)
-              command ${lang}env \"\$cmd\" \"\$@\"
-            ;;
-          esac
-        }
-        function __${lang}env_manpath() {
-          local cur=\$(${lang}env version-name)
-          if [[ "\$cur" != "\$__cur_${lang}env_version" ]]; then
-            local mpath
-            for mpath (~/.${lang}env/versions/*(/)) manpath=(\"\${(@)manpath:#\$mpath/share/man}\")
-            manpath[1,0]=(\"$HOME/.${lang}env/versions/\$cur/share/man\"(/))
-          fi
-          __cur_${lang}env_version=\$cur
-        }"
-        eval "$funs"
-        chpwd_functions+=(__${lang}env_manpath)
-      fi
-    fi
-  done
 }
