e4665cccb5138c574c31a4d50c72712201103940
Chris Hoffman <cehoffman@gmail.com>
2020-09-16 16:36:22 -0500

    Use gpg command to find ssh agent socket

--- zsh/envs/gpg.zsh
+++ zsh/envs/gpg.zsh
@@ -2,6 +2,6 @@ if [[ -d ~/.gnupg ]]; then
   if (( $+commands[gpgconf] )); then
     export GPG_TTY=$(tty)
     gpgconf --launch gpg-agent # This requires at least 2.1
-    export SSH_AUTH_SOCK="${GNUPGHOME-$HOME/.gnupg}/S.gpg-agent.ssh"
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
   fi
 fi
