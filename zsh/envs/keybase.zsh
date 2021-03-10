3c0fb9244586cb4db2bed591f08dfae5b6dc0688
Chris Hoffman <cehoffman@gmail.com>
2021-03-10 00:34:18 -0600

    Support multiple paths for homebrew keybase

--- zsh/envs/keybase.zsh
+++ zsh/envs/keybase.zsh
@@ -1,3 +1,1 @@
-if [[ -d /Applications/Keybase.app/Contents/SharedSupport/bin ]]; then
-  path[1,0]=/Applications/Keybase.app/Contents/SharedSupport/bin
-fi
+path[1,0]=(/Applications/Keybase.app/Contents/SharedSupport/bin(/) ~/Applications/Keybase.app/Contents/SharedSupport/bin(/))
