0ea207532734c13dc688a25245887d73b3e21724
Chris Hoffman <cehoffman@gmail.com>
2021-03-11 14:26:43 -0600

    Don't bother with auth sock sync in gpg-agent mode

--- zsh/envs/ssh-agent.zsh
+++ zsh/envs/ssh-agent.zsh
@@ -1,4 +1,4 @@
-if [[ -o interactive && -n $TMUX ]]; then
+if [[ -o interactive && -n "${TMUX}" && -z "${GPG_TTY}" ]]; then
   function sync-ssh-auth-sock() {
     local NEW_SSH_AUTH_SOCK="$(command tmux showenv| grep \^SSH_AUTH_SOCK | cut -d = -f 2)"
     if [[ -n "$NEW_SSH_AUTH_SOCK" && -S "$NEW_SSH_AUTH_SOCK" ]]; then
