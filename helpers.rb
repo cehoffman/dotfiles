526be90b0060d8bc64fd50ae2086f87d2c2c58ba
Chris Hoffman <cehoffman@gmail.com>
2021-03-12 00:57:48 -0600

    Remove many no longer used configurations

--- helpers.rb
+++ helpers.rb
@@ -1,2 +1,2 @@
-def windows?; RUBY_PLATFORM =~ /(mswin|mingw)32/ end
-def mac?; RUBY_PLATFORM =~ /darwin/ end
+def windows?; !!(RUBY_PLATFORM =~ /(mswin|mingw)32/) end
+def mac?; !!(RUBY_PLATFORM =~ /darwin/) end
