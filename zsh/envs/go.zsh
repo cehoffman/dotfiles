54cb77ee280bdac074f0f4714a63c19993bab506
Chris Hoffman <cehoffman@gmail.com>
2021-05-22 00:47:54 -0500

    Further speedup shell start removing unneeded calls

--- zsh/envs/go.zsh
+++ zsh/envs/go.zsh
@@ -1,8 +1,6 @@
 if (( $+commands[go] )); then
   export GOPATH=~/Projects/gocode
   path[2,1]=$GOPATH:q/bin
-  for prog ($GOPATH:q/bin/*(.:tx)) alias ${prog}="env -u DYLD_INSERT_LIBRARIES $prog"
-  unset prog
   export GONOPROXY=git.vertiv.life
   export GONOSUMDB=git.vertiv.life
 fi
--- zsh/envs/gpg.zsh
+++ zsh/envs/gpg.zsh
@@ -1,7 +1,6 @@
-if [[ -d ~/.gnupg ]]; then
-  if (( $+commands[gpgconf] )); then
-    export GPG_TTY=$(tty)
-    gpgconf --launch gpg-agent # This requires at least 2.1
-    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-  fi
+if [[ -d ~/.gnupg ]] && (( $+commands[gpgconf] )); then
+  export GPG_TTY=$(tty)
+  gpgconf --launch gpg-agent # This requires at least 2.1
+  # export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+  export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
 fi
