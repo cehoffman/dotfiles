647efc638c31556fb31dd0e0216d986acab5b022
Chris Hoffman <cehoffman@gmail.com>
2024-02-18 18:48:40 -0600

    Trigger Obsidian plugin in vim to load from entry commands

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -1,24 +1,25 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",
-  ft = "markdown",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    workspaces = {
-      {
-        name = "world",
-        path = "/Users/christopherhoffman/Library/Mobile Documents/com~apple~CloudDocs/World",
-      },
-    },
-    notes_subdir = "3 - Resources/notes",
-    mappings = {
-      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-      ["gf"] = {
-        action = function()
-          return require("obsidian").util.gf_passthrough()
-        end,
-        opts = { noremap = false, expr = true, buffer = true, desc = "Follow link" },
-      },
-    },
-  },
+	"epwalsh/obsidian.nvim",
+	version = "*",
+	ft = "markdown",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	cmd = { "ObsidianToday", "ObsidianNew", "ObsidianOpen", "ObsidianSearch", "ObsidianWorkspace" },
+	opts = {
+		workspaces = {
+			{
+				name = "world",
+				path = "/Users/christopherhoffman/Library/Mobile Documents/com~apple~CloudDocs/Obsidian/World",
+			},
+		},
+		notes_subdir = "3 - Resources/notes",
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true, desc = "Follow link" },
+			},
+		},
+	},
 }
