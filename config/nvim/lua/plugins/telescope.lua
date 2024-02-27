9fea3436dd906a00aca21f6f2d530502b87ab906
Chris Hoffman <cehoffman@gmail.com>
2024-02-27 01:13:47 -0600

    Change ,p to open finder at git root, but show untracked files

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -19,7 +19,19 @@ return {
 			desc = "Switch Buffer",
 		},
 		{ "<leader>,", false },
-		{ "<leader>p", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+		{
+			"<leader>p",
+			function(...)
+				local root_dir = vim.fs.find(".git", { path = vim.loop.cwd(), upward = true })[1]
+				if root_dir then
+					root_dir = vim.fs.dirname(root_dir)
+				else
+					root_dir = false
+				end
+				require("lazyvim.util").telescope("files", vim.tbl_deep_extend("force", ... or {}, { cwd = root_dir }))()
+			end,
+			desc = "Find files (git root or local)",
+		},
 		-- { '<leader>s', "<cmd>Telescope registers<cr>", desc = "Registers" },
 	},
 }
